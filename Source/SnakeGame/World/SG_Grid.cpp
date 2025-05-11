b9d6932e8094aadefa29b63de80a5b16fa4d6f60
DESKTOP-E7L2H89\Kroniks <wowmandarin@yandex.ru>
2025-05-12 02:26:47 +0300

    Implemented grid view

--- Source/SnakeGame/World/SG_Grid.cpp
+++ Source/SnakeGame/World/SG_Grid.cpp
@@ -1,4 +1,8 @@
 #include "World/SG_Grid.h"
+#include "Core/Grid.h"
+#include "DrawDebugHelpers.h"
+
+DEFINE_LOG_CATEGORY_STATIC(LogWorldGrid, All, All);
 
 
 ASG_Grid::ASG_Grid()
@@ -13,7 +17,15 @@ void ASG_Grid::BeginPlay()
 
 void ASG_Grid::SetModel(const TSharedPtr<Snake::Grid>& grid, int32 cellSize)
 {
+	if (grid.IsValid() == false)
+	{
+		UE_LOG(LogWorldGrid, Fatal, TEXT("Grid is null, game aborted!"));
+	}
 
+	GridDimensions = grid.Get()->dimensions();
+	CellSize = cellSize;
+	WorldWidth = CellSize * GridDimensions.width;
+	WorldHeight = CellSize * GridDimensions.height;
 }
 
 void ASG_Grid::Tick(float DeltaTime)
@@ -24,5 +36,35 @@ void ASG_Grid::Tick(float DeltaTime)
 
 void ASG_Grid::DrawGrid()
 {
+	for (int32 i = 0; i < GridDimensions.height + 1; ++i)
+	{
+		const FVector startLocation = GetActorLocation() + GetActorForwardVector() * CellSize * i;
+		DrawDebugLine
+		(
+			GetWorld(),
+			startLocation,
+			startLocation + GetActorRightVector() * CellSize * WorldWidth,
+			FColor::Green,
+			false,
+			-1.0f,
+			0U,
+			1.0f
+		);
+	}
 	
+	for (int32 i = 0; i < GridDimensions.width + 1; ++i)
+	{
+		const FVector startLocation = GetActorLocation() + GetActorRightVector() * CellSize * i;
+		DrawDebugLine
+		(
+			GetWorld(),
+			startLocation,
+			startLocation + GetActorForwardVector() * CellSize * WorldHeight,
+			FColor::Red,
+			false,
+			-1.0f,
+			0U,
+			1.0f
+		);
+	}
 }
